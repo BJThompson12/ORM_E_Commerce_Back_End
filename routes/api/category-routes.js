de523c04d98f078866a77b40221b1815d87eb648
Brandon Thompson <brandonjoel.thompson@gmail.com>
2023-03-25 21:01:19 -0400

    category post api completed

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -24,13 +24,25 @@ router.get('/:id', async (req, res) => {
        include: [{ model: Product}],
     });
     res.status(200).json(categoryData);
+
     } catch (err) {
       res.status(500).json(err);
     }
   });
 
-router.post('/', (req, res) => {
   // create a new category
+router.post('/', async (req, res) => {
+   /* req.body for add (the id will be assigned)
+    {
+      "category_name": "Pants"
+    }
+  */
+    try {
+      const newCategory = await Category.create(req.body);
+      res.status(200).json(newCategory);
+    } catch (err) {
+      res.status(400).json(err);
+    };
 });
 
 router.put('/:id', (req, res) => {
